307e075a1e24fe06ae05aab63ac14f06213a7176
MIbarra16 <46502600+MIbarra16@users.noreply.github.com>
2022-02-18 17:50:59 -0700

    Add files via upload

--- LowCostBioreactorGUI.py
+++ LowCostBioreactorGUI.py
@@ -5,7 +5,7 @@ from xml.etree.ElementTree import SubElement
 import xml.etree.ElementTree as ET
 from tkinter import filedialog as fd
 import threading 
-#from w1thermsensor import W1ThermSensor
+from w1thermsensor import W1ThermSensor
 import time
 from tkinter import messagebox
 import datetime 
@@ -228,38 +228,48 @@ def Customrun():
 #        timerlabel.config(text = timer )
 #        root.after(1000, timerf, total_seconds-1)
 def TempD():
+    sensor = W1ThermSensor()
     global TempR
-   #os.system('modprobe w1-gpio')
-   #os.system('modprobe w1-therm')
- 
-    base_dir = '/sys/bus/w1/devices/'
-    device_folder = glob.glob(base_dir + '28*')[0]
-    device_file = device_folder + '/w1_slave'
     Templabel = Label(top4, text = "Tempature in Celsius")
     Templabel.pack() 
     TempR = Label(top4, text = '' )
     TempR.pack()
-    top4.after(1000,read_temp(device_file))
+    top4.after(1000,TempC)  
+def TempC():
+    TempReading = sensor.get.tempature()
+    TempR.config(text = TempReading + 'celsius'
+
+   #os.system('modprobe w1-gpio')
+   #os.system('modprobe w1-therm')
+ 
+#    base_dir = '/sys/bus/w1/devices/'
+#    device_folder = glob.glob(base_dir + '28*')[0]
+#    device_file = device_folder + '/w1_slave'
+#    Templabel = Label(top4, text = "Tempature in Celsius")
+#    Templabel.pack() 
+#    TempR = Label(top4, text = '' )
+#    TempR.pack()
+#    top4.after(1000,read_temp(device_file))
  
 
-def read_temp_raw(device_file):
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
+#def read_temp_raw(device_file):
+#    f = open(device_file, 'r')
+#    lines = f.readlines()
+#    f.close()
+#    return lines
  
-def read_temp(device_file):
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw(device_file)
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        TempR.config(text = temp_c)
-        return temp_c, temp_f
+#def read_temp(device_file):
+#    lines = read_temp_raw()
+#    while lines[0].strip()[-3:] != 'YES':
+#        time.sleep(0.2)
+#        lines = read_temp_raw(device_file)
+#    equals_pos = lines[1].find('t=')
+#    if equals_pos != -1:
+#        temp_string = lines[1][equals_pos+2:]
+#        temp_c = float(temp_string) / 1000.0
+#        temp_f = temp_c * 9.0 / 5.0 + 32.0
+#        TempR.config(text = temp_c)
+#        return temp_c, temp_f
     
 def TimerD(): 
     hour=StringVar()
